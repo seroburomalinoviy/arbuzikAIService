e0d8d331bb9d0158318b0b1577c1a8fc0fd8a8ea
grideperlevy <mr.ivandmi@yandex.ru>
2023-12-19 19:36:38 +0300

    add test for models

--- database_app/admin.py
+++ database_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import User
-# Register your models here.
+from .models import User, Subscription
+
 admin.site.register(User)
+admin.site.register(Subscription)
