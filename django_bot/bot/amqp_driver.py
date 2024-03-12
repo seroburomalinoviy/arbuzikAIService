1f128a128b0fc51f3adff731b5bba114ea6341ac
grideperlevy <mr.ivandmi@yandex.ru>
2024-03-12 10:38:48 +0300

    add logs

--- django_bot/bot/amqp_driver.py
+++ django_bot/bot/amqp_driver.py
@@ -6,7 +6,18 @@ import aio_pika
 load_dotenv()
 
 logger = logging.getLogger(__name__)
+import sentry_sdk
 
+sentry_sdk.init(
+    dsn="https://674f9f3c6530ddb20607dd9a42413fa4@o4506896610885632.ingest.us.sentry.io/4506896620978176",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
 
 async def push_amqp_message(user_id, pitch, voice_path):
     connection = await aio_pika.connect_robust(
@@ -15,6 +26,7 @@ async def push_amqp_message(user_id, pitch, voice_path):
         login=os.environ.get('RABBIT_USER'),
         password=os.environ.get('RABBIT_PASSWORD'),
     )
+    logger.info('Connected to rabbit')
     queue_name = "bot-to-rvc"
     routing_key = "bot-to-rvc"
     payload = str(user_id) + '_' + str(voice_path) + '_' + str(pitch)
@@ -26,6 +38,7 @@ async def push_amqp_message(user_id, pitch, voice_path):
             aio_pika.Message(body=payload.encode()),
             routing_key=routing_key,
         )
+    logger.info(f'message{payload} sent to rabbit')
 
 
 async def amqp_listener():
@@ -35,6 +48,7 @@ async def amqp_listener():
         login=os.environ.get('RABBIT_USER'),
         password=os.environ.get('RABBIT_PASSWORD'),
     )
+    logger.info(f'Connected to rabbit')
 
     queue_name = "rvc-to-bot"
 
@@ -51,7 +65,7 @@ async def amqp_listener():
         async with queue.iterator() as queue_iter:
             async for message in queue_iter:
                 async with message.process():
-                    logger.info(f'Message I got: {message.body}')
+                    logger.info(f'bot got msg from rabbit: {message.body}')
 
                     if queue.name in message.body.decode():
                         break
