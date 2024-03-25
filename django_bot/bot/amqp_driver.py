690f338e584b986206230d1ae0d7d2c5635d0934
grideperlevy <mr.ivandmi@yandex.ru>
2024-03-25 23:00:45 +0300

    debug

--- django_bot/bot/amqp_driver.py
+++ django_bot/bot/amqp_driver.py
@@ -29,6 +29,8 @@ async def send_answer(message):
     voice_id = payload.get('voice_id')
     voice_path = os.environ.get('USER_VOICES_PROCESSED_VOLUME') + '/' + voice_id
 
+    await bot.send_message(chat_id=chat_id, text='Получай сука')
+
     await bot.sendVoice(chat_id=chat_id, voice=voice_path)
 
 
