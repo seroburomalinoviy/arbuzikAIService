d66a214b500402bbf64f143d60b1d67b13439ff9
grideperlevy <mr.ivandmi@yandex.ru>
2024-04-02 15:56:50 +0300

    parser init

---
+++ django_bot/parser/main.py
@@ -0,0 +1,26 @@
+import os
+
+import pandas
+
+# читать файл
+
+
+def read_file(filename):
+    df = pandas.DataFrame()
+    data = df.read_csv()
+
+
+def parser(apps, schema_editor):
+    filename = os.environ.get("")
+    data = read_file(read_file)
+    Subcription = apps.get_model("bot", "Subscription")
+    # for ...
+        #
+        # subscription = Subcription.objects.create(
+        #     image_cover=
+        #     days_limit=
+        #     price=
+        #     time_voice_limit=
+        #     title=
+        # )
+
