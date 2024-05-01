8de5e18b79e5735cef7e5793abf7c780ff574140
grideperlevy <mr.ivandmi@yandex.ru>
2024-05-01 15:30:45 +0300

    add parser

--- django_bot/bot/models.py
+++ django_bot/bot/models.py
@@ -1,5 +1,3 @@
-import os
-
 from dotenv import load_dotenv
 
 from django.db import models
@@ -17,67 +15,6 @@ class OverwriteStorage(FileSystemStorage):
     def get_available_name(self, name, max_length=None):
         self.delete(name)
         return super().get_available_name(name, max_length)
-
-
-class User(models.Model):
-    telegram_id = models.CharField(
-        'telegram id',
-        max_length=250,
-        primary_key=True,
-    )
-    telegram_nickname = models.CharField(
-        'telegram nick name',
-        max_length=100,
-        blank=True,
-        null=True,
-    )
-    telegram_username = models.CharField(
-        'telegram username',
-        max_length=100,
-        blank=True,
-        null=True,
-    )
-    subscription_status = models.BooleanField(
-        'Статус подписки',
-        editable=True, 
-        default=False
-    )
-    subscription_final_date = models.DateTimeField(
-        'Дата окончания подписки',
-        editable=True, 
-        null=True,
-    )
-    subscription_attempts = models.PositiveIntegerField(
-        'Количество попыток',
-        validators=[MinValueValidator(0)],
-        editable=True, 
-        null=True
-    )
-    pitch = models.IntegerField(
-        'Pitch',
-        default=0,
-        validators=[MinValueValidator(-150), MaxValueValidator(150)],  # todo check NN
-        blank=True,
-        editable=True,
-        null=True
-    )
-    subscription = models.ForeignKey(
-        'Subscription',
-        on_delete=models.SET_DEFAULT,
-        default=os.environ.get('DEFAULT_SUBSCRIPTION'),
-        related_name='users'
-    )
-    favorites = models.ManyToManyField(
-        'Voice',  
-        blank=True
-    )
-
-    def __str__(self):
-        return self.telegram_nickname
-
-    class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
 
 
 class Subscription(models.Model):
---
+++ django_bot/user/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+import os
+
+
+class User(models.Model):
+    telegram_id = models.CharField(
+        'telegram id',
+        max_length=250,
+        primary_key=True,
+    )
+    telegram_nickname = models.CharField(
+        'telegram nick name',
+        max_length=100,
+        blank=True,
+        null=True,
+    )
+    telegram_username = models.CharField(
+        'telegram username',
+        max_length=100,
+        blank=True,
+        null=True,
+    )
+    subscription_status = models.BooleanField(
+        'Статус подписки',
+        editable=True,
+        default=False
+    )
+    subscription_final_date = models.DateTimeField(
+        'Дата окончания подписки',
+        editable=True,
+        null=True,
+    )
+    subscription_attempts = models.PositiveIntegerField(
+        'Количество попыток',
+        validators=[MinValueValidator(0)],
+        editable=True,
+        null=True
+    )
+    pitch = models.IntegerField(
+        'Pitch',
+        default=0,
+        validators=[MinValueValidator(-150), MaxValueValidator(150)],  # todo check NN
+        blank=True,
+        editable=True,
+        null=True
+    )
+    subscription = models.ForeignKey(
+        'Subscription',
+        on_delete=models.SET_DEFAULT,
+        default=os.environ.get('DEFAULT_SUBSCRIPTION'),
+        related_name='users'
+    )
+    # favorites = models.ManyToManyField(
+    #     'Voice',
+    #     blank=True
+    # )
+
+    def __str__(self):
+        return self.telegram_nickname
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
