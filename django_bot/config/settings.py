268f3adb0366c26d765a25c29b1702330e45ba6b
grideperlevy <mr.ivandmi@yandex.ru>
2024-05-03 20:36:02 +0300

    implement slug for voice and demka in bot

--- django_bot/config/settings.py
+++ django_bot/config/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = f"http://{os.environ.get('HOST')}/"
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
