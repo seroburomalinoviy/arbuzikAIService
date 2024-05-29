c7e1bc78349dff16248e9918dcc72512111e152b
grideperlevy <mr.ivandmi@yandex.ru>
2024-05-29 22:40:28 +0300

    make fixing conversationhandlers mixed up: test

--- django_bot/bot/handlers/main.py
+++ django_bot/bot/handlers/main.py
@@ -260,7 +260,7 @@ async def inline_query(update: Update, context: ContextTypes.DEFAULT_TYPE):
             )
         )
     await update.inline_query.answer(results, cache_time=100, auto_pagination=True)
-    return ConversationHandler.END
+    return VOICE_PROCESSING
 
 
 @log_journal
