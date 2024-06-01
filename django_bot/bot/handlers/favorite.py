b5bc533a97fea1523ba27f2c871b672d4fb55201
grideperlevy <mr.ivandmi@yandex.ru>
2024-06-01 13:55:22 +0300

    fix bug with paths to models pth and indx

---
+++ django_bot/bot/handlers/favorite.py
@@ -0,0 +1,16 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+
+from bot.logic.utils import get_object, filter_objects, log_journal, save_model
+from bot.
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@log_journal
+async def favorite_add(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.callback_query.answer()
+
+    return BASE_STATES
