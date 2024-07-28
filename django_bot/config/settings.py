709f7f1675ac3b5ed9747d05f068b41c5ee25671
grideperlevy <mr.ivandmi@yandex.ru>
2024-07-28 18:51:25 +0300

    fix proccess

--- django_bot/config/settings.py
+++ django_bot/config/settings.py
@@ -88,7 +88,8 @@ ASGI_APPLICATION = "config.asgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        # "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
