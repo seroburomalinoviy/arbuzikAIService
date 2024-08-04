5e63d7635dda6bb0359dd2e7233596c1f7322767
grideperlevy <mr.ivandmi@yandex.ru>
2024-08-04 17:40:38 +0300

    fix async filter

--- django_bot/bot/handlers/main.py
+++ django_bot/bot/handlers/main.py
@@ -136,7 +136,7 @@ async def category_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     context.user_data['subscription_name'] = subscription_name
     context.user_data['subscription_status'] = subscription_status
 
-    categories = await sync_to_async(Category.objects.filter(subscription__title=subscription_name), thread_sensitive=True)
+    categories = await sync_to_async(Category.objects.filter(subscription__title=subscription_name))
     len_cat = len(categories)
 
     # Кнопки Поиск по всем голосам и Избранное
