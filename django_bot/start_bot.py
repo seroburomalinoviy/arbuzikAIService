eb7dc65a4d37b4098c0525afa8c3f6d3b3a289f0
grideperlevy <mr.ivandmi@yandex.ru>
2024-08-05 16:08:28 +0300

    info bot

--- django_bot/start_bot.py
+++ django_bot/start_bot.py
@@ -16,7 +16,7 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 def main() -> None:
     load_dotenv()
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s >>> %(funcName)s(%(lineno)d)",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
