c0973e6be9d4e171a37465f431c6d09df07f3d8a
grideperlevy <mr.ivandmi@yandex.ru>
2024-08-07 09:56:37 +0300

    add migration files

--- django_bot/config/settings.py
+++ django_bot/config/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = f"http://{os.environ.get('HOST')}/"
 MEDIA_ROOT = BASE_DIR / 'media'
 
