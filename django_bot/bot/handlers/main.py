5e6479df3e6cbf7b03d8f3eca94d1fd75886d7d9
grideperlevy <mr.ivandmi@yandex.ru>
2024-08-08 21:03:53 +0300

    fix async

--- django_bot/bot/handlers/main.py
+++ django_bot/bot/handlers/main.py
@@ -324,7 +324,7 @@ async def voice_set(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     slug_voice = context.user_data.get('slug_voice')
     voice = await Voice.objects.aget(slug=slug_voice)
-    user = await User.objects.aget(telegram_id=query.from_user.id).select_related('subscription')
+    user = await User.objects.select_related('subscription').aget(telegram_id=query.from_user.id)
 
     if not valid_subscription(user):
         user.subscription_status = False
