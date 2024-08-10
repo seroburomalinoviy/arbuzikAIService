39acf9d45cb642fdeacfdc4f802f7de72719ee08
grideperlevy <mr.ivandmi@yandex.ru>
2024-08-10 09:40:46 +0300

    add backup volume

--- backup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker exec -i container_name pg_dump --username pg_username [--password pg_password] db_name > /path/on/your/machine/dump.sql
-pg_dump -h db -U "$POSTGRES_USER" "$POSTGRES_DB" > /backups/db_backup_"$(date +%F_%H-%M-%S)".sql
