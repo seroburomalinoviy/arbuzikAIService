a52e916eaf875f54730b8eb199e3484bc208ba0b
grideperlevy <mr.ivandmi@yandex.ru>
2024-08-14 22:00:03 +0300

    rabbit multi queue

--- client/main.py
+++ client/main.py
@@ -128,7 +128,6 @@ async def reader(channel: aioredis.client.PubSub):
                     logger.debug(payload)
 
                     push_amqp_message(payload)
-                    push_amqp_message('rvc-to-bot')
 
                 await asyncio.sleep(0.01)
         except asyncio.TimeoutError:
