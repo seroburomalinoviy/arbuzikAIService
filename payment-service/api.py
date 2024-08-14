9046b0bb1b9cee4e29baab3bf5b671ec55db5093
grideperlevy <mr.ivandmi@yandex.ru>
2024-08-14 19:28:36 +0300

    set nginx

--- payment-service/api.py
+++ payment-service/api.py
@@ -1,7 +1,7 @@
 from ampq_driver import push_amqp_message
 
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, responses
 
 app = FastAPI(redoc_url=None)
 
@@ -10,9 +10,11 @@ app = FastAPI(redoc_url=None)
 async def get_payment(order_id: Union[str, None] = None, amount: Union[str, None] = None, currency: Union[str, None] = None):
     await push_amqp_message({'order_id': order_id, 'amount': amount, 'currency': currency, 'success': True},
                             routing_key='payment-to-bot')
+    return responses.Response(status_code=200)
 
 
 @app.get('/payment/failure')
 async def get_payment(order_id: Union[str, None] = None, amount: Union[str, None] = None, currency: Union[str, None] = None):
     await push_amqp_message({'order_id': order_id, 'amount': amount, 'currency': currency, 'success': False},
                             routing_key='payment-to-bot')
+    return responses.Response(status_code=200)
