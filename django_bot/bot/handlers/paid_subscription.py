cfecf1f725f32322cd4db201ca586bc26bb5fcc9
grideperlevy <mr.ivandmi@yandex.ru>
2024-08-15 15:21:03 +0300

    chore: change text message for payment url

--- django_bot/bot/handlers/paid_subscription.py
+++ django_bot/bot/handlers/paid_subscription.py
@@ -20,89 +20,6 @@ from bot.logic.constants import *
 from bot.logic.amqp_driver import push_amqp_message
 
 
-# @log_journal
-# async def offer_vip_subscription(update, context):
-#     chat_id = (
-#         update.effective_chat.id
-#         if update.message
-#         else update.callback_query.message.chat.id
-#     )
-#     subscription_title = "violetvip"
-#     subscription = await Subscription.objects.aget(title=subscription_title)
-#
-#     await context.bot.send_photo(
-#         chat_id=chat_id,
-#         photo=open(
-#             str(settings.MEDIA_ROOT) + "/" + str(subscription.image_cover), "rb"
-#         ),
-#     )
-#
-#     await context.bot.send_message(
-#         chat_id=chat_id,
-#         text=message_text.offer_vip_subscription_text,
-#         parse_mode=ParseMode.MARKDOWN,
-#         reply_markup=InlineKeyboardMarkup(
-#             [
-#                 [
-#                     InlineKeyboardButton(
-#                         f" 💵 Разовый платёж - {subscription.price} руб",
-#                         callback_data=f"payment_{subscription.price}_{subscription.title}",
-#                     )
-#                 ],
-#                 [
-#                     InlineKeyboardButton(
-#                         "▶️ Другие подписки", callback_data="paid_subscriptions"
-#                     ),
-#                     InlineKeyboardButton(
-#                         "⏩ Вернуться в меню", callback_data="category_menu"
-#                     ),
-#                 ],
-#             ]
-#         ),
-#     )
-
-
-# @log_journal
-# async def offer_subscriptions(update: Update, context):
-#     chat_id = (
-#         update.effective_chat.id
-#         if update.message
-#         else update.callback_query.message.chat.id
-#     )
-#     keyboard = list()
-#     async for sub in Subscription.objects.exclude(title=os.environ.get("DEFAULT_SUBSCRIPTION")).all().order_by("price"):
-#         sub_title = sub.title
-#         keyboard.append(
-#             [
-#                 InlineKeyboardButton(
-#                     sub.telegram_title, callback_data=f"paid_subscription_{sub_title}"
-#                 )
-#             ]
-#         )
-#
-#     keyboard.append(
-#         [InlineKeyboardButton("⏩ Вернуться в меню", callback_data="category_menu")]
-#     )
-#
-#     demo_sub = await Subscription.objects.aget(
-#         title=os.environ.get("DEFAULT_SUBSCRIPTION")
-#     )
-#
-#     await context.bot.send_photo(
-#         chat_id=chat_id,
-#         photo=open(
-#             str(settings.MEDIA_ROOT) + "/" + str(demo_sub.image_cover), "rb"
-#         ),  # в демо подписке лежит специальная картинка
-#     )
-#
-#     await context.bot.send_message(
-#         chat_id=chat_id,
-#         text=message_text.offer_subscription_text,
-#         parse_mode=ParseMode.MARKDOWN,
-#         reply_markup=InlineKeyboardMarkup(keyboard),
-#     )
-
-
 @log_journal
 async def show_paid_subscriptions(update: Update, context: ContextTypes.DEFAULT_TYPE, offer=None):
     """
@@ -117,8 +34,6 @@ async def show_paid_subscriptions(update: Update, context: ContextTypes.DEFAULT_
         button_text = "⏩ Вернуться в меню"
         message = message_text.offer_subscription_text
     else:
-        # query = update.callback_query
-        # await query.answer()
         message = message_text.all_paid_subs
         button_text = "⏩ Перейти к выбору голосов"
 
@@ -181,8 +96,6 @@ async def preview_paid_subscription(update: Update, context: ContextTypes.DEFAUL
         title = subscription_title
         message = message_text.offer_vip_subscription_text
     else:
-        # query = update.callback_query
-        # await query.answer()
         title = update.callback_query.data.split("paid_subscription_")[1]
         message = None
 
