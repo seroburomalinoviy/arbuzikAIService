56fe6d9e23b3a86c2b303fb3e495baf9e12dff66
grideperlevy <mr.ivandmi@yandex.ru>
2024-08-16 20:17:56 +0300

    canel removing voice

--- django_bot/bot/logic/amqp_driver.py
+++ django_bot/bot/logic/amqp_driver.py
@@ -128,10 +128,13 @@ async def send_rvc_answer(data):
         reply_markup=InlineKeyboardMarkup(keyboards.final_buttons),
     )
 
-    tmp_path = os.environ.get("USER_VOICES") + "/" + "tmp/"
-    os.makedirs(tmp_path, exist_ok=True)
-    os.replace(file_path, tmp_path + audio.voice_filename)
-    os.replace(file_path + ".tmp", tmp_path + audio.voice_filename + '.tmp')
+    os.remove(file_path)
+    os.remove(file_path + ".tmp")
+
+    # tmp_path = os.environ.get("USER_VOICES") + "/" + "tmp/"
+    # os.makedirs(tmp_path, exist_ok=True)
+    # os.replace(file_path, tmp_path + audio.voice_filename)
+    # os.replace(file_path + ".tmp", tmp_path + audio.voice_filename + '.tmp')
 
     logger.info("Voice files removed")
 
