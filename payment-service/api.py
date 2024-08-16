7b181a55e1f31cd23afa366dbf5c80f7b70ecb47
grideperlevy <mr.ivandmi@yandex.ru>
2024-08-16 09:09:54 +0300

    feat: ip detrminator

--- payment-service/api.py
+++ payment-service/api.py
@@ -20,26 +20,24 @@ app = FastAPI(redoc_url=None)
 
 
 @app.get('/check')
-async def check(request: Request, remote_ip: str = Header(None, alias='X-Real-IP')):
-    return HTMLResponse(content=f"Success: your ip: {remote_ip}", status_code=200)
+async def check():
+    return HTMLResponse(content="<h1><b><i><u>Success</u></i></b></h1>", status_code=200)
 
 
 @app.post('/payment')
-async def get_payment(request: Request) -> Response:
+async def get_payment(request: Request, remote_ip: str = Header(None, alias='X-Real-IP')) -> Response:
     f = await request.form()
     json_f = jsonable_encoder(f)
     payment = ApiPayment(json_f)
 
     logger.info(f'{json_f=}')
 
-    ip_request: str = request.client.host
-    logger.info(f'{ip_request=}')
     ips_allowed: list = await get_actual_ips()
-    logger.info(f'{ips_allowed=}')
+    logger.info(f'{ips_allowed=}, {remote_ip=}')
 
-    if ip_request not in ips_allowed:
+    if remote_ip not in ips_allowed:
         logger.warning('Not allowed ip!')
-        # return Response(status_code=400)
+        return Response(status_code=400)
 
     secret_key_2 = os.environ.get('SECRET_KEY_2')
     key = f'{payment.merchant_id}:{payment.amount}:{payment.currency}:{secret_key_2}:{payment.order_id}'
@@ -47,7 +45,7 @@ async def get_payment(request: Request) -> Response:
 
     if internal_sign != payment.sign:
         logger.warning('No appropriated sign!')
-        # return Response(status_code=400)
+        return Response(status_code=400)
 
     data = {
         'order_id': payment.order_id,
