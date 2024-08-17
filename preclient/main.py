1d5435f913582ea05ce9d0f624271d995552deb1
grideperlevy <mr.ivandmi@yandex.ru>
2024-08-17 13:15:37 +0300

    logging preclient

--- preclient/main.py
+++ preclient/main.py
@@ -5,39 +5,33 @@ import os
 from dotenv import load_dotenv
 import aio_pika
 
-logger = logging.getLogger(__name__)
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s >>> %(funcName)s(%(lineno)d)",
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
 
 load_dotenv()
 
 
 async def create_task(payload):
     # https://aioredis.readthedocs.io/en/latest/getting-started/
-    logger.debug(f"redis input args: {payload}")
+    logging.info(f"redis input args: {payload}")
+
     redis = aioredis.from_url(
-        url=f"redis://{os.environ.get('REDIS_HOST')}"  # TODO: можем ли использовать сеть докеров? Оптимально?
+        url=f"redis://{os.environ.get('REDIS_HOST')}"
     )
     pubsub = redis.pubsub()
     await pubsub.subscribe("channel:raw-data")
     await redis.publish("channel:raw-data", payload)
 
-    logger.debug("message send to redis")
+    logging.info("message send to redis")
 
 
 async def task_listener():
-    logger.debug("Start task listener")
+    logging.debug("Start task listener")
     connection = await aio_pika.connect_robust(
         host=os.environ.get("RABBIT_HOST"),
         port=int(os.environ.get("RABBIT_PORT")),
         login=os.environ.get("RABBIT_USER"),
         password=os.environ.get("RABBIT_PASSWORD"),
     )
-    logger.debug("Connection with rabbitmq established")
+    logging.debug("Connection with rabbitmq established")
 
     queue_name = "bot-to-rvc"
 
@@ -45,11 +39,12 @@ async def task_listener():
         channel = await connection.channel()
         await channel.set_qos(prefetch_count=10)
         queue = await channel.declare_queue(queue_name, durable=True, auto_delete=True)
-        logger.debug(f"get queue {queue_name} from rabbit")
         async with queue.iterator() as queue_iter:
             async for message in queue_iter:
                 async with message.process():
-                    logger.info(f"message.body.decode() = {message.body.decode()}")
+
+                    logging.info(f"preclient get message {queue_name=}: {message.body.decode()}")
+
                     await create_task(message.body.decode())
 
                     if queue.name in message.body.decode():
@@ -57,6 +52,12 @@ async def task_listener():
 
 
 if __name__ == "__main__":
-    # Почему не создавать один коннекшн и использовать его
-    # Сейчас для каждой записи создается конекшн и даже не закрывается
+    os.makedirs('/logs', exist_ok=True)
+    rotating_handler = logging.handlers.RotatingFileHandler('/logs/preclient.log', backupCount=5, maxBytes=512 * 1024)
+    log_format = "%(asctime)s - %(levelname)s - %(name)s - %(message)s >>> %(funcName)s(%(lineno)d)"
+    formatter = logging.Formatter(log_format)
+    rotating_handler.setFormatter(formatter)
+    logging.basicConfig(level=logging.INFO, format=log_format, datefmt="%Y-%m-%d %H:%M:%S")
+    logging.getLogger('').addHandler(rotating_handler)
+    
     asyncio.run(task_listener())
