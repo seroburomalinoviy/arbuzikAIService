feaa69bbfc968be4b0d400a9855835d3cdbe66f5
grideperlevy <mr.ivandmi@yandex.ru>
2024-08-17 19:55:28 +0300

    backup

--- backup.sh
+++ backup.sh
@@ -2,5 +2,5 @@
 
 source .env
 
-docker exec -i postgres pg_dump -U "$DB_USER" "$DB_NAME" > "$BACKUP_HOST_PATH"/db_backup_"$(date +%F_%H-%M-%S)".sql
 find "$BACKUP_HOST_PATH"/* -mtime +1 -exec rm {} \;
+docker exec -i postgres pg_dump -U "$DB_USER" "$DB_NAME" > "$BACKUP_HOST_PATH"/db_backup_"$(date +%F_%H-%M-%S)".sql
