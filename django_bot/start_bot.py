146c42ca46b181537c71768b335f3d1c38f34459
grideperlevy <mr.ivandmi@yandex.ru>
2024-08-17 14:33:22 +0300

    lexper log

--- django_bot/start_bot.py
+++ django_bot/start_bot.py
@@ -1,4 +1,4 @@
-import logging
+# import logging
 import os
 from dotenv import load_dotenv
 import asyncio
@@ -10,17 +10,70 @@ from bot.logic.setup import init_handlers
 from bot.logic.amqp_driver import amqp_payment_url_listener, amqp_payment_listener, amqp_rvc_listener
 
 
+
+def start_rotating_logging(path):
+    import logging
+    from logging.handlers import RotatingFileHandler
+
+    log_format = "%(asctime)s - %(levelname)s - %(name)s - %(message)s >>> %(funcName)s(%(lineno)d)"
+    logging.basicConfig(level=logging.INFO, format=log_format, datefmt="%Y-%m-%d %H:%M:%S")
+
+    handler = RotatingFileHandler(path, backupCount=5, maxBytes=512 * 1024)
+    handler.setFormatter(logging.Formatter(log_format))
+
+    logger = logging.getLogger(__name__)
+    logger.addHandler(handler)
+
+    class CustomFilter(logging.Filter):
+        COLOR = {
+            "DEBUG": "GREEN",
+            "INFO": "GREEN",
+            "WARNING": "YELLOW",
+            "ERROR": "RED",
+            "CRITICAL": "RED",
+        }
+
+        def filter(self, record):
+            record.color = CustomFilter.COLOR[record.levelname]
+            return True
+
+    logger.addFilter(CustomFilter())
+
+
+
+# class CustomFilter(logging.Filter):
+#     COLOR = {
+#         "DEBUG": "GREEN",
+#         "INFO": "GREEN",
+#         "WARNING": "YELLOW",
+#         "ERROR": "RED",
+#         "CRITICAL": "RED",
+#     }
+#
+#     def filter(self, record):
+#         record.color = CustomFilter.COLOR[record.levelname]
+#         return True
+#
+# logging.basicConfig(
+#     level=logging.DEBUG,
+#     format="%(asctime)s - [%(levelname)s] - [%(color)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s",
+# )
+#
+#
+# logger = logging.getLogger(__name__)
+# logger.addFilter(CustomFilter())
+
+
 def main() -> None:
     load_dotenv()
-    logging.getLogger("httpx").setLevel(logging.WARNING)
 
-    os.makedirs('/logs', exist_ok=True)
-    rotating_handler = logging.handlers.RotatingFileHandler('/logs/bot.log', backupCount=5, maxBytes=512 * 1024)
-    log_format = "%(asctime)s - %(levelname)s - %(name)s - %(message)s >>> %(funcName)s(%(lineno)d)"
-    formatter = logging.Formatter(log_format)
-    rotating_handler.setFormatter(formatter)
-    logging.basicConfig(level=logging.INFO, format=log_format, datefmt="%Y-%m-%d %H:%M:%S")
-    logging.getLogger(__name__).addHandler(rotating_handler)
+    # os.makedirs('/logs', exist_ok=True)
+    # rotating_handler = logging.handlers.RotatingFileHandler('/logs/bot.log', backupCount=5, maxBytes=512 * 1024)
+    # log_format = "%(asctime)s - %(levelname)s - %(name)s - %(message)s >>> %(funcName)s(%(lineno)d)"
+    # formatter = logging.Formatter(log_format)
+    # rotating_handler.setFormatter(formatter)
+    # logging.basicConfig(level=logging.INFO, format=log_format, datefmt="%Y-%m-%d %H:%M:%S")
+    # logging.getLogger('').addHandler(rotating_handler)
 
     TOKEN = os.environ.get("BOT_TOKEN")
     application = ApplicationBuilder().token(TOKEN).build()
@@ -37,4 +90,8 @@ def main() -> None:
 
 
 if "__main__" == __name__:
+    os.makedirs('/logs', exist_ok=True)
+    start_rotating_logging(path="/logs/bot.log")
+    logging.getLogger("httpx").setLevel(logging.WARNING)
+
     main()
