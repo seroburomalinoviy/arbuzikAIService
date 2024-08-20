cba8d6a7364185df79f5504ad5e96f6b4f8bab36
grideperlevy <mr.ivandmi@yandex.ru>
2024-08-20 18:18:05 +0300

    CELERY: update command for removing

--- django_bot/bot/tasks.py
+++ django_bot/bot/tasks.py
@@ -5,7 +5,7 @@ import logging
 
 @shared_task
 def clean_user_voices():
-    os.remove(f'{os.environ.get("USER_VOICES")}/*')
+    os.system(f'rm -rf {os.environ.get("USER_VOICES")}/')
     logging.info('User voices was cleaned up')
 
 
