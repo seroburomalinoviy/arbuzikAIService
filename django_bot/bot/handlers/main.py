2383cef1e9a95c6fc0993dad39714a5b925ac50d
grideperlevy <mr.ivandmi@yandex.ru>
2024-08-20 12:47:15 +0300

    ref

--- django_bot/bot/handlers/main.py
+++ django_bot/bot/handlers/main.py
@@ -138,12 +138,13 @@ async def subscription(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
 
     if is_member.status in allowed_user_statuses:
+        await query.answer()
         system_voice = await Voice.objects.aget(slug=os.environ.get('SYSTEM_VOICE'))
         demka_path = system_voice.demka.path
         demka_cover = system_voice.demka_image
         if demka_path:
             if demka_cover:
-                answer = await context.bot.send_audio(
+                await context.bot.send_audio(
                     chat_id=query.from_user.id,
                     audio=open(demka_path, "rb"),
                     filename=system_voice.title,
@@ -151,19 +152,17 @@ async def subscription(update: Update, context: ContextTypes.DEFAULT_TYPE):
                     duration=60
                 )
             else:
-                answer = await context.bot.send_audio(
+                await context.bot.send_audio(
                     chat_id=query.from_user.id,
                     audio=open(demka_path, "rb"),
                     filename=system_voice.title,
                     duration=60
                 )
-        logging.info(f'{answer=}')
-        if answer:
-            await query.answer()
-            await query.edit_message_text(
-                message_text.demo_rights,
-                reply_markup=InlineKeyboardMarkup(keyboards.is_subscribed),
-            )
+
+        await query.edit_message_text(
+            message_text.demo_rights,
+            reply_markup=InlineKeyboardMarkup(keyboards.is_subscribed),
+        )
 
         return BASE_STATES
     elif is_member.status in unresolved_user_statuses:
