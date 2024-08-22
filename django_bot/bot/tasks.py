493463c06d45c9b15c1391f240fdb8737658e4a1
grideperlevy <mr.ivandmi@yandex.ru>
2024-08-22 14:16:48 +0300

    CELERY: change logic

--- django_bot/bot/tasks.py
+++ django_bot/bot/tasks.py
@@ -5,9 +5,9 @@ import logging
 
 @app.task()
 def clean_user_voices():
-    logging.info('START TASK')
+    # logging.info('START TASK')
     print('hauhai')
-    os.system(f'rm -rf {os.environ.get("USER_VOICES")}/')
-    logging.info('User voices was cleaned up')
+    os.remove(f'{os.environ.get("USER_VOICES")}/*')
+    # logging.info('User voices was cleaned up')
 
 
