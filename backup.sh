9a3ad7b8366aaf1c304d2726128306b33fa7dfdf
grideperlevy <mr.ivandmi@yandex.ru>
2024-08-24 07:38:46 +0300

    change tj find 0 day older files

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source .env
-find "$BACKUP_HOST_PATH"/* -mtime +1 -exec rm {} \;
+find "$BACKUP_HOST_PATH"/* -mtime +0 -exec rm {} \;
 docker exec -i postgres pg_dump -U "$DB_USER" "$DB_NAME" > "$BACKUP_HOST_PATH"/db_backup_"$(date +%F_%H-%M-%S)".sql
