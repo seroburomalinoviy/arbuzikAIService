bcfc0f666fffb7390f6aa4c82ae42876278eb45f
grideperlevy <mr.ivandmi@yandex.ru>
2024-09-27 09:56:55 +0300

    change env postgres names

--- django_bot/config/settings.py
+++ django_bot/config/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": os.environ.get("POSTGRES_NAME"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("POSTGRES_HOST", "127.0.0.1"),
+        "HOST": os.environ.get("POSTGRES_DB", "127.0.0.1"),
         "PORT": os.environ.get("POSTGRES_PORT", 5432),
     }
 }
