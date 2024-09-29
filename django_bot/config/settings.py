d5b1faa4c863f0c30d964dd85500725b75690a6d
grideperlevy <mr.ivandmi@yandex.ru>
2024-09-29 15:39:29 +0300

    csrf setting

--- django_bot/config/settings.py
+++ django_bot/config/settings.py
@@ -51,9 +51,9 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     # "django.middleware.csrf.CsrfViewMiddleware",
-    # "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
