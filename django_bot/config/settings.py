ec9b2dcebdc39c118402c55eaaaded13e320146a
grideperlevy <mr.ivandmi@yandex.ru>
2024-09-29 14:20:40 +0300

    csrf setting

--- django_bot/config/settings.py
+++ django_bot/config/settings.py
@@ -143,8 +143,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-CSRF_TRUSTED_ORIGINS = [f"http://{os.environ.get('HOST')}"]
+ALLOWED_ORIGINS = ['http://*', 'https://*']
+CSRF_TRUSTED_ORIGINS = ALLOWED_ORIGINS.copy()
+# CSRF_TRUSTED_ORIGINS = [f"http://{os.environ.get('HOST')}"]
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_HTTPONLY = True
