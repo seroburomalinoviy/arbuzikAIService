490888695ec63e0e2bc573bf5fde7ec204ea349b
grideperlevy <mr.ivandmi@yandex.ru>
2024-11-11 12:33:02 +0300

    set nginx for prometheus

--- preclient/prometheus_server.py
+++ preclient/prometheus_server.py
@@ -8,8 +8,8 @@ load_dotenv()
 
 
 if __name__ == "__main__":
-    start_http_server("8010")
-    # start_http_server(os.environ.get("PROMETHEUS_PORT"))
+    # start_http_server(int(os.environ.get("PROMETHEUS_PORT")))
+    start_http_server(8010)
     logging.info(f"Prometheus server started")
 
     # app = make_asgi_app()
