d582b0334066f39453ec3a0df17a02757f5d1c51
grideperlevy <mr.ivandmi@yandex.ru>
2024-11-13 21:46:59 +0300

    prometheus: python library server

--- preclient/prometheus_server.py
+++ preclient/prometheus_server.py
@@ -1,5 +1,5 @@
-from prometheus_client import make_wsgi_app
-from wsgiref.simple_server import make_server
+from prometheus_client import make_asgi_app
+
 import os
 from dotenv import load_dotenv
 import logging
@@ -11,9 +11,9 @@ load_dotenv()
 
 if __name__ == "__main__":
     # start_http_server(int(os.environ.get("PROMETHEUS_PORT")))
-    app = make_wsgi_app()
-    httpd = make_server('', 9090, app)
-    httpd.serve_forever()
+
+    app = make_asgi_app(disable_compression=True)
+    asyncio.run(app)
     logging.info(f"Prometheus server started")
 
     # app = make_asgi_app()
