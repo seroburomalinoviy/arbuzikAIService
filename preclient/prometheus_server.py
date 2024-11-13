eec59cbd750dd141e6a76f09d9fba602b42a49c2
grideperlevy <mr.ivandmi@yandex.ru>
2024-11-13 23:11:39 +0300

    prometheus: python library server

--- preclient/prometheus_server.py
+++ preclient/prometheus_server.py
@@ -21,21 +21,21 @@
 #     })
 #     logging.info(f"Prometheus server started")
 
-from flask import Flask
+from fastapi import FastAPI
 from prometheus_client import make_wsgi_app, Counter, Gauge
 
-app = Flask(__name__)
+app = FastAPI(redoc_url=None)
 
 REQUEST_COUNT = Counter('app_requests_total', 'Total request count')
 CURRENT_REQUESTS = Gauge('app_requests_inprogress', 'Requests currently in progress')
 
-@app.route('/')
+@app.get('/')
 def hello():
   REQUEST_COUNT.inc()
 
   with CURRENT_REQUESTS.track_inprogress():
     return 'Hello World!'
 
-if __name__ == "__main__":
-  app.wsgi_app = make_wsgi_app(app.wsgi_app)
-  app.run(port=9090)
+# if __name__ == "__main__":
+#   app.wsgi_app = make_wsgi_app(app.wsgi_app)
+#   app.run(port=9090)
