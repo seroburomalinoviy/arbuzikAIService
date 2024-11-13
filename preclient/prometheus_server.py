bb946d4e17b0d50dab5e1ea262794977f2d5534e
grideperlevy <mr.ivandmi@yandex.ru>
2024-11-13 22:39:59 +0300

    prometheus: python library server

--- preclient/prometheus_server.py
+++ preclient/prometheus_server.py
@@ -1,23 +1,41 @@
+# from flask import Flask
+# from werkzeug.middleware.dispatcher import DispatcherMiddleware
+# from prometheus_client import make_wsgi_app
+# 
+# import os
+# from dotenv import load_dotenv
+# import logging
+# import asyncio
+# 
+# 
+# load_dotenv()
+# 
+# 
+# if __name__ == "__main__":
+#     # start_http_server(int(os.environ.get("PROMETHEUS_PORT")))
+# 
+#     app = Flask(__name__)
+#     # Add prometheus wsgi middleware to route /metrics requests
+#     app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+#         '/metrics': make_wsgi_app()
+#     })
+#     logging.info(f"Prometheus server started")
+
 from flask import Flask
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from prometheus_client import make_wsgi_app
+from prometheus_client import make_wsgi_app, Counter, Gauge
 
-import os
-from dotenv import load_dotenv
-import logging
-import asyncio
+app = Flask(__name__)
 
+REQUEST_COUNT = Counter('app_requests_total', 'Total request count')
+CURRENT_REQUESTS = Gauge('app_requests_inprogress', 'Requests currently in progress')
 
-load_dotenv()
+@app.route('/')
+def hello():
+  REQUEST_COUNT.inc()
 
+  with CURRENT_REQUESTS.track_inprogress():
+    return 'Hello World!'
 
 if __name__ == "__main__":
-    # start_http_server(int(os.environ.get("PROMETHEUS_PORT")))
-
-    app = Flask(__name__)
-    # Add prometheus wsgi middleware to route /metrics requests
-    app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
-        '/metrics': make_wsgi_app()
-    })
-    logging.info(f"Prometheus server started")
-
+  app.wsgi_app = make_wsgi_app(app.wsgi_app)
+  app.run(port=9090)
