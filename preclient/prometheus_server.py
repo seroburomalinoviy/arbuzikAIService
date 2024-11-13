21edd039ecb4c6af449c275eabfa7493b0015462
grideperlevy <mr.ivandmi@yandex.ru>
2024-11-13 23:42:08 +0300

    add prometheus scraper

--- preclient/prometheus_server.py
+++ preclient/prometheus_server.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
-from prometheus_client import Gauge
+from prometheus_client import Gauge, make_asgi_app
 
-app = FastAPI(redoc_url=None)
+app = FastAPI(debug=False)
 
 # REQUEST_COUNT = Counter('app_requests_total', 'Total request count')
 # CURRENT_REQUESTS = Gauge('app_requests_inprogress', 'Requests currently in progress')
 RAW_TASKS = Gauge('raw_tasks', 'Description of gauge')
 
-
-@app.get('/metrics')
-def hello():
-    return RAW_TASKS
+# Add prometheus asgi middleware to route /metrics requests
+metrics_app = make_asgi_app()
+app.mount("/metrics", metrics_app)
 
