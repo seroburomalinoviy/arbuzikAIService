f545dfede76d3b1fce4a8493935541c3ddc2bbf5
grideperlevy <mr.ivandmi@yandex.ru>
2024-11-27 12:38:20 +0300

    chore: use models

--- django_bot/bot/structures/schemas.py
+++ django_bot/bot/structures/schemas.py
@@ -1,7 +1,7 @@
 import json
 import os
 from telegram import Bot
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from typing import ClassVar
 
 
@@ -20,24 +20,6 @@ class RVCData(BaseModel):
     bot: ClassVar = Bot(token=os.environ.get("BOT_TOKEN"))
 
 
-# class RVCData:
-#     def __init__(self, arg):
-#         data = json.loads(arg)
-#         self.voice_model_index = data['voice_model_index']
-#         self.voice_model_pth = data['voice_model_pth']
-#         self.pitch = data['pitch']
-#         self.extension = data['extension']
-#         self.voice_name = data['voice_name']
-#         self.chat_id = data['chat_id']
-#         self.user_id = data['user_id']
-#         self.voice_title = data['voice_title']
-#         self.duration = data['duration']
-#         self.voice_filename = data['voice_filename']
-#         self.message_id = data['message_id']
-#         self.image = data.get("image")
-#         self.bot = Bot(token=os.environ.get("BOT_TOKEN"))
-
-
 class Payment(BaseModel):
     order_id: int
     amount: int
@@ -48,26 +30,13 @@ class Payment(BaseModel):
     service: str
     bot: ClassVar = Bot(token=os.environ.get("BOT_TOKEN"))
 
-# class Payment:
-#     def __init__(self, arg):
-#         data = json.loads(arg)
-#         self.order_id = data['order_id']
-#         self.amount = data['amount']
-#         self.currency = data['currency']
-#         self.merchant_id = data['merchant_id']
-#         self.order_id = data['order_id']
-#         self.status = data['status']
-#         self.service = data['service']
-#         self.bot = Bot(token=os.environ.get("BOT_TOKEN"))
 
+class PayUrl(BaseModel):
+    type: str = ''
+    url: str = ''
+    subscription_title: str = ''
+    order_id: int
+    amount: int
+    chat_id: str = ''
+    bot: ClassVar = Bot(token=os.environ.get("BOT_TOKEN"))
 
-class PayUrl:
-    def __init__(self, arg):
-        data = json.loads(arg)
-        self.type = data['type']
-        self.url = data['url']
-        self.subscription_title = data['subscription_title']
-        self.order_id = data['order_id']
-        self.amount = data['amount']
-        self.chat_id = data['chat_id']
-        self.bot = Bot(token=os.environ.get("BOT_TOKEN"))
