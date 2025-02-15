fda580e9673b4a5165bf3aeb78f414ec82684dc3
grideperlevy <mr.ivandmi@yandex.ru>
2025-02-15 16:24:31 +0300

    add logging to preclient

--- preclient/main.py
+++ preclient/main.py
@@ -10,11 +10,5 @@ import logging_config
 if __name__ == "__main__":
     os.makedirs('/logs', exist_ok=True)
     dictConfig(logging_config.config)
-    # handler = RotatingFileHandler('/logs/preclient.log', backupCount=5, maxBytes=512 * 1024)
-    # log_format = "%(asctime)s - %(levelname)s - %(name)s - %(message)s >>> %(funcName)s(%(lineno)d)"
-    # formatter = logging.Formatter(log_format)
-    # handler.setFormatter(formatter)
-    # logging.basicConfig(level=logging.INFO, format=log_format, datefmt="%Y-%m-%d %H:%M:%S")
-    # logging.getLogger('').addHandler(handler)
 
     asyncio.run(amqp_listener("bot-to-rvc", create_task))
