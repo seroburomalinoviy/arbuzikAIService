56f957ea8aedb59a631a3cabfe0d1efec75bbd9a
grideperlevy <mr.ivandmi@yandex.ru>
2025-02-15 16:24:19 +0300

    add logging to preclient

--- preclient/main.py
+++ preclient/main.py
@@ -1,19 +1,20 @@
 import asyncio
-import logging
-from logging.handlers import RotatingFileHandler
+from logging.config import dictConfig
 import os
 
 from amqp.driver import amqp_listener
 from amqp.tasks import create_task
+import logging_config
 
 
 if __name__ == "__main__":
     os.makedirs('/logs', exist_ok=True)
-    handler = RotatingFileHandler('/logs/preclient.log', backupCount=5, maxBytes=512 * 1024)
-    log_format = "%(asctime)s - %(levelname)s - %(name)s - %(message)s >>> %(funcName)s(%(lineno)d)"
-    formatter = logging.Formatter(log_format)
-    handler.setFormatter(formatter)
-    logging.basicConfig(level=logging.INFO, format=log_format, datefmt="%Y-%m-%d %H:%M:%S")
-    logging.getLogger('').addHandler(handler)
+    dictConfig(logging_config.config)
+    # handler = RotatingFileHandler('/logs/preclient.log', backupCount=5, maxBytes=512 * 1024)
+    # log_format = "%(asctime)s - %(levelname)s - %(name)s - %(message)s >>> %(funcName)s(%(lineno)d)"
+    # formatter = logging.Formatter(log_format)
+    # handler.setFormatter(formatter)
+    # logging.basicConfig(level=logging.INFO, format=log_format, datefmt="%Y-%m-%d %H:%M:%S")
+    # logging.getLogger('').addHandler(handler)
 
     asyncio.run(amqp_listener("bot-to-rvc", create_task))
