ce4b51301a3f3a24b1b8c760da93df10c7cf6cdf
grideperlevy <mr.ivandmi@yandex.ru>
2025-04-10 22:46:35 +0300

    chabge way closing connection

--- django_bot/bot/tasks.py
+++ django_bot/bot/tasks.py
@@ -10,7 +10,6 @@ import os
 import traceback
 
 from bot.structures.schemas import Payment
-from bot.logic.utils import connection
 
 load_dotenv()
 
@@ -29,7 +28,6 @@ def clean_user_voices():
     logger.info('User voices was cleaned up')
     return True
 
-@connection
 @app.task(ignore_result=False)
 def check_pay_aaio(order_id: str):
     AAIO_INFO = os.environ.get("AAIO_INFO")
@@ -126,7 +124,6 @@ def check_pay_aaio(order_id: str):
                     logger.info('Celery task was successfully sent to rabbitmq')
                 return msg
 
-@connection
 @app.task(ignore_result=False)
 def check_pay_ukassa(order_id: str, payment_id: str, timer:int):
     SERVICE = 'ukassa'
